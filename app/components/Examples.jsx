ba4bbd3e365646ac6b773c29ff6b046cbfbc893e
Tomás Andrés Flores Alvear <tomfloresa@Tomass-MBP.home>
2017-05-04 22:21:03 -0300

    keeping foundation

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,16 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
-    <h3>Examples</h3>
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few examples to try out</p>
+      <ol>
+        <li><Link to="/?location=Philadelphia">Philadelphia, PA</Link></li>
+        <li><Link to="/?location=Rio">Rio, BR</Link></li>
+      </ol>
+    </div>
   );
 }
 
